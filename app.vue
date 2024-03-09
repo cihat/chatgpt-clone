c3a7faf8f2ba058393723c065e3706a3d7097bcc
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-03-09 17:52:29 +0300

    Change to nuxt for server side actions

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import "@/assets/main.css"
+import ChatSidebar from "@/components/Chat/ChatSidebar.vue"
+import ChatWindow from "@/components/Chat/ChatWindow.vue"
+</script>
+
+<template>
+  <div class="chat-container">
+    <chat-sidebar />
+    <chat-window />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chat-container {
+  display: flex;
+  height: 100vh;
+  width: 100%;
+}
+</style>
